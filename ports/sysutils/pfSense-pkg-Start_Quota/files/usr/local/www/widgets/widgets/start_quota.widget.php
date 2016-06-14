196b42e60ae7c26c09f261cf4e96a15b1d8135ea
Derek Battams <derek@battams.ca>
2016-06-14 09:23:57 -0400

    use ajax to load widget data

--- ports/sysutils/pfSense-pkg-Start_Quota/files/usr/local/www/widgets/widgets/start_quota.widget.php
+++ ports/sysutils/pfSense-pkg-Start_Quota/files/usr/local/www/widgets/widgets/start_quota.widget.php
@@ -16,40 +16,20 @@
 */
 require_once("guiconfig.inc");
 require_once('/usr/local/www/widgets/include/start_quota.inc');
-$xml = new SimpleXMLElement(getXmlData());
 ?>
-<div style="padding: 2px">
-	<table width="100%" id="ups_widget" summary="Start.ca Usage">
-		<tbody>
-			<tr>
-				<th>Type</th>
-				<th>Download</th>
-				<th>Upload</th>
-				<th>Total</th>
-			</tr>
-			<tr>
-				<td>Billed</td>
-				<td><?= formatBytes($xml->used->download) ?></td>
-				<td><?= formatBytes($xml->used->upload) ?></td>
-				<td><?= formatBytes($xml->used->download + $xml->used->upload) ?></td>
-			</tr>
-                        <tr>
-                                <td>Free</td>
-                                <td><?= formatBytes($xml->grace->download) ?></td>
-                                <td><?= formatBytes($xml->grace->upload) ?></td>
-                                <td><?= formatBytes($xml->grace->download + $xml->grace->upload) ?></td>
-                        </tr>
-                        <tr>
-                                <td><b>TOTAL</b></td>
-                                <td><b><?= formatBytes($xml->total->download) ?></b></td>
-                                <td><b><?= formatBytes($xml->total->upload) ?></b></td>
-                                <td><b><?= formatBytes($xml->total->download + $xml->total->upload) ?></b></td>
-                        </tr>
-			<?php
-				if(getQuota() > 0)
-					echo '<tr><td colspan="4"><b>Quota Remaining: ' . formatBytes(getQuota() - $xml->used->download - $xml->used->upload) . '</b></td></tr>';
-				echo '<tr><td colspan="4"><i>Data current as of ' . date('D, j M Y G:i T', getDataDate()) . '</i></td></tr>';
-			?>
-		</tbody>
-	</table>
+<div id="start_quota_container" style="padding: 2px">
+	<p><b><i>Loading data...</i></b></p>
 </div>
+<script>
+events.push(
+	function() {
+		var ajax = $.ajax("/start_quota_ajax.php")
+			.done(function(data) {
+				$("#start_quota_container").html(data);
+			})
+			.fail(function() {
+				$("#start_quota_container").html("<b>Error loading data!</b>");
+			});
+	}
+);
+</script>
